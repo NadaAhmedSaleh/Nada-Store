081502e93c47f03fd6a134ee2c520901722585c9
NadaAhmedSaleh <nada.ahmedsaleh98@gmail.com>
2023-12-17 12:57:33 +0200

    add fake resonse for the end point

--- src/service/api.js
+++ src/service/api.js
@@ -1,3 +1,5 @@
+import products from "../data";
+
 const BASE_URL = "https://fakestoreapi.com/";
 
 export const getProducts = async (limit) => {
@@ -5,7 +7,9 @@ export const getProducts = async (limit) => {
     const response = await fetch(`${BASE_URL}products?limit=${limit}}`);
     return await response.json();
   } catch (error) {
-    console.error("Error fetching products:", error);
-    throw error;
+    /* this is fake response incase any failure happened in the fetching from 
+     the fake end point to be able to check the design
+    as i found it is sometimes not stable*/
+    return products;
   }
 };
